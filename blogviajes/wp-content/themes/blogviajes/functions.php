c81a854397d83e1da14b6003fabbad409c5a5bbf
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2019-05-09 01:29:52 -0500

    Añadí el Footer con sus Respectivos Widgets y Menu de Redes Sociales
    iconos con FontAwesome y se Instalo la librería SASS para el Style.CSS

--- blogviajes/wp-content/themes/blogviajes/functions.php
+++ blogviajes/wp-content/themes/blogviajes/functions.php
@@ -14,8 +14,23 @@ add_action('wp_enqueue_scripts', 'blogviajes_styles');
 
 //Navegación
 register_nav_menus(array(
-   'menu_principal' => __('Menu Principal', 'blogViajes')
+   'menu_principal' => __('Menu Principal', 'blogViajes'),
+   'sociales_menu' => __('Menu Redes Sociales', 'blogViajes')
 ));
+
+//Widget
+function blogviajes_widgets(){
+    register_sidebar(array(
+        'name' => __('Footer Widgets'),
+        'id' => 'footer_widget',
+        'description' => 'Widgets para el Footer',
+        'before_widget' => '<div id="%1$s" class="widget col-sm-6 %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ));
+}
+add_action('widgets_init', 'blogviajes_widgets');
 
 //Imagen Destacada
 add_theme_support('post-thumbnails');
