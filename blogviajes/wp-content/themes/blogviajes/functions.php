e415b397d3b2829acf5c10bab076a2c40dbd5b3a
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2019-05-09 01:30:22 -0500

    listo el footer

--- blogviajes/wp-content/themes/blogviajes/functions.php
+++ blogviajes/wp-content/themes/blogviajes/functions.php
@@ -2,9 +2,9 @@
 function blogviajes_styles() {
     wp_enqueue_style('normalize', get_stylesheet_directory_uri().'/css/normalize.css' );
     wp_enqueue_style('bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");
-    wp_enqueue_style('Raleway', "https://fonts.googleapis.com/css?family=Raleway:900");
-    wp_enqueue_style('Baskerville', "https://fonts.googleapis.com/css?family=Libre+Baskerville:700");
-    wp_enqueue_style('Fontawesome', "https://use.fontawesome.com/releases/v5.8.2/css/all.css");
+    wp_enqueue_style('raleway', "https://fonts.googleapis.com/css?family=Raleway:900");
+    wp_enqueue_style('baskerville', "https://fonts.googleapis.com/css?family=Libre+Baskerville:700");
+    wp_enqueue_style('fontawesome', "https://use.fontawesome.com/releases/v5.8.2/css/all.css");
     wp_enqueue_style('style', get_stylesheet_uri());
 
     wp_enqueue_script('jquery');
