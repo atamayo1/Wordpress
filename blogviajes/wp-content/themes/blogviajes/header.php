d5b6f59251082f963c90af25ec119514e1772478
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2019-05-10 18:46:39 -0500

    Agregue la opción de Defaulth Theme para las paginas
    con  la opción de poner el Full Width

--- blogviajes/wp-content/themes/blogviajes/header.php
+++ blogviajes/wp-content/themes/blogviajes/header.php
@@ -53,10 +53,12 @@
                    <div class="col-md-6">
                        <div class="titulo">
                             <?php if(is_front_page()): ?>
-                            <?php $description = get_bloginfo( 'description', 'display' ); ?>
-                            <h1 class="site-title"><span> <?php echo $description; ?> </span></h1>
+                                <?php $description = get_bloginfo( 'description', 'display' ); ?>
+                                <h1 class="site-title"><span> <?php echo $description; ?> </span></h1>
+                            <?php elseif(is_category()): ?>
+                                <h1 class="site-title"><span> <?php the_archive_title(); ?> </span></h1>
                             <?php else: ?>
-                            <h1 class="site-title"><span> <?php the_title(); ?> </span></h1>
+                                <h1 class="site-title"><span> <?php the_title(); ?> </span></h1>
                             <?php endif; ?>
                         </div><!--.titulo-->
                    </div><!--.col-md-6-->
---
+++ blogviajes/wp-content/themes/blogviajes/page.php
@@ -0,0 +1,21 @@
+<?php
+get_header();
+?>
+
+<section class="container-fluid contenido">
+    <div class="row">
+        <?php if(is_page('contacto')):?>
+            <div class="col-sm-8 col-sm-offset-2 pagina-full-width">
+                <?php 
+                    while(have_posts()): the_post();
+                    the_content();
+                    endwhile;
+                ?>
+            </div>
+            <?php else: ?>
+                <?php the_content() ?>
+       <?php endif;?>
+    </div>
+</section>
+
+<?php get_footer(); ?>
