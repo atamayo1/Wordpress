893a7c83fa4c299f728323bf40e73d27c16640c5
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2019-05-10 15:16:51 -0500

    Agregue las estructuras de las paginas de las Entradas con Single.php

--- blogviajes/wp-content/themes/blogviajes/single.php
+++ blogviajes/wp-content/themes/blogviajes/single.php
@@ -1,12 +1,12 @@
-<?php
-get_header();
-?>
+<?php get_header();?>
 
 <section class="container contenido">
     <div class="row">
 
        <?php 
-       
+       while (have_posts()): the_post();
+            the_content();
+       endwhile;
        ?>
        
     </div>
